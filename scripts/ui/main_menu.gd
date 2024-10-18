b4d605e5bc771df2d559166627f6fe13e9069799
GaetanoRe <gaetanore@live.com>
2024-10-17 22:40:19 -0400

    initialized repo

---
+++ scripts/ui/main_menu.gd
@@ -0,0 +1,20 @@
+extends Control
+
+
+
+
+func _on_play_pressed():
+	var main_scene = load("res://scenes/main_scene.tscn")
+	self.get_tree().change_scene_to_packed(main_scene)
+
+
+	
+
+
+func _on_settings_pressed():
+	print("Settings has been pressed")
+
+
+
+func _on_quit_pressed():
+	self.get_tree().quit()
